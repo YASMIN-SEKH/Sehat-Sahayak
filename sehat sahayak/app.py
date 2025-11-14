a57d961ef5c478adc91be211095c284f4eea1726
YASMIN <yasmin0sekh0@gmail.com>
2025-11-14 16:45:50 +0530

    deployed locally

--- sehat sahayak/app.py
+++ sehat sahayak/app.py
@@ -2,15 +2,21 @@ from flask import Flask, render_template
 from heart_disease import heart_bp
 from diabetes import diabetes_bp
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-# Register Blueprints
-app.register_blueprint(heart_bp, url_prefix='/heart')
-app.register_blueprint(diabetes_bp, url_prefix='/diabetes')
+    # Register Blueprints
+    app.register_blueprint(heart_bp, url_prefix='/heart')
+    app.register_blueprint(diabetes_bp, url_prefix='/diabetes')
 
-@app.route('/')
-def main():
-    return render_template('main.html')
+    @app.route('/')
+    def main():
+        return render_template('main.html')
 
+    return app
+
+
+# Run the Flask app
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True)
